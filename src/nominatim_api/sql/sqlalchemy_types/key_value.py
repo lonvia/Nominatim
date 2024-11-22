9f59c01fc4549eed163f1af1852f36f03160f2b5
Sarah Hoffmann <lonvia@denofr.de>
2024-11-22 12:37:37 +0100

    simplify custom KeyValue SQL type
    Implement a separate Comperator for each underlying type using the
    respective default Comperator classes as base.

--- src/nominatim_api/sql/sqlalchemy_types/key_value.py
+++ src/nominatim_api/sql/sqlalchemy_types/key_value.py
@@ -10,7 +10,6 @@ A custom type that implements a simple key-value store of strings.
 from typing import Any
 
 import sqlalchemy as sa
-from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.dialects.postgresql import HSTORE
 from sqlalchemy.dialects.sqlite import JSON as sqlite_json
 
@@ -25,37 +24,25 @@ class KeyValueStore(sa.types.TypeDecorator[Any]):
 
     def load_dialect_impl(self, dialect: SaDialect) -> sa.types.TypeEngine[Any]:
         if dialect.name == 'postgresql':
-            return HSTORE()  # type: ignore[no-untyped-call]
+            self.comparator_factory = self.hstore_comparator_factory  # type: ignore
+            return dialect.type_descriptor(HSTORE())  # type: ignore[no-untyped-call]
 
-        return sqlite_json(none_as_null=True)
+        return dialect.type_descriptor(sqlite_json(none_as_null=True))
 
-    class comparator_factory(sa.types.UserDefinedType.Comparator):  # type: ignore[type-arg]
+    class hstore_comparator_factory(HSTORE.Comparator):
 
         def merge(self, other: SaColumn) -> 'sa.Operators':
             """ Merge the values from the given KeyValueStore into this
                 one, overwriting values where necessary. When the argument
                 is null, nothing happens.
             """
-            return KeyValueConcat(self.expr, other)
+            return self.expr.op('||')(sa.func.coalesce(other, sa.cast('', HSTORE)))
 
+    class comparator_factory(sqlite_json.Comparator):  # type: ignore[type-arg]
 
-class KeyValueConcat(sa.sql.expression.FunctionElement[Any]):
-    """ Return the merged key-value store from the input parameters.
-    """
-    type = KeyValueStore()
-    name = 'JsonConcat'
-    inherit_cache = True
-
-
-@compiles(KeyValueConcat)
-def default_json_concat(element: KeyValueConcat, compiler: 'sa.Compiled', **kw: Any) -> str:
-    arg1, arg2 = list(element.clauses)
-    return "(%s || coalesce(%s, ''::hstore))"\
-        % (compiler.process(arg1, **kw), compiler.process(arg2, **kw))
-
-
-@compiles(KeyValueConcat, 'sqlite')
-def sqlite_json_concat(element: KeyValueConcat, compiler: 'sa.Compiled', **kw: Any) -> str:
-    arg1, arg2 = list(element.clauses)
-    return "json_patch(%s, coalesce(%s, '{}'))"\
-        % (compiler.process(arg1, **kw), compiler.process(arg2, **kw))
+        def merge(self, other: SaColumn) -> 'sa.Operators':
+            """ Merge the values from the given KeyValueStore into this
+                one, overwriting values where necessary. When the argument
+                is null, nothing happens.
+            """
+            return sa.func.json_patch(self.expr, sa.func.coalesce(other, '{}'), type_=sqlite_json)
