5e2dad86834e41eb43caccd0c7f4254e08e2b8ab
Sarah Hoffmann <lonvia@denofr.de>
2025-08-05 20:53:05 +0200

    include restrict tokens in search

--- src/nominatim_api/search/db_search_lookups.py
+++ src/nominatim_api/search/db_search_lookups.py
@@ -21,7 +21,9 @@ LookupType = sa.sql.expression.FunctionElement[Any]
 
 class LookupAll(LookupType):
     """ Find all entries in search_name table that contain all of
-        a given list of tokens using an index for the search.
+        a given list of single word tokens using an index for the search.
+
+        The tokens must be a list of partials without stop words.
     """
     inherit_cache = True
 
@@ -60,26 +62,33 @@ def _sqlite_lookup_all(element: LookupAll,
 class LookupAny(LookupType):
     """ Find all entries that contain at least one of the given tokens.
         Use an index for the search.
+
+        Tokens can be partials and full words. Only use on name.
     """
     inherit_cache = True
 
     def __init__(self, table: SaFromClause, column: str, tokens: List[int]) -> None:
-        super().__init__(table.c.place_id, getattr(table.c, column), column,
+        super().__init__(table.c.place_id,
+                         getattr(table.c, column),
+                         getattr(table.c, f"restrict_{column}"),
+                         column,
                          sa.type_coerce(tokens, IntArray))
 
 
 @compiles(LookupAny)
 def _default_lookup_any(element: LookupAny,
                         compiler: 'sa.Compiled', **kw: Any) -> str:
-    _, col, _, tokens = list(element.clauses)
-    return "(%s && %s)" % (compiler.process(col, **kw),
-                           compiler.process(tokens, **kw))
+    _, col, restrict_col, _, tokens = list(element.clauses)
+    return "(array_cat(%s, %s) && %s)" % (
+        compiler.process(col, **kw),
+        compiler.process(restrict_col, **kw),
+        compiler.process(tokens, **kw))
 
 
 @compiles(LookupAny, 'sqlite')
 def _sqlite_lookup_any(element: LookupAny,
                        compiler: 'sa.Compiled', **kw: Any) -> str:
-    place, _, colname, tokens = list(element.clauses)
+    place, _, _, colname, tokens = list(element.clauses)
     return "%s IN (SELECT CAST(value as bigint) FROM"\
            " (SELECT array_union(places) as p FROM reverse_search_name"\
            "   WHERE word IN (SELECT value FROM json_each('[' || %s || ']'))"\
@@ -97,18 +106,24 @@ class Restrict(LookupType):
 
     def __init__(self, table: SaFromClause, column: str, tokens: List[int]) -> None:
         super().__init__(getattr(table.c, column),
+                         getattr(table.c, f"restrict_{column}"),
                          sa.type_coerce(tokens, IntArray))
 
 
 @compiles(Restrict)
 def _default_restrict(element: Restrict,
                       compiler: 'sa.Compiled', **kw: Any) -> str:
-    arg1, arg2 = list(element.clauses)
-    return "(coalesce(null, %s) @> %s)" % (compiler.process(arg1, **kw),
-                                           compiler.process(arg2, **kw))
+    col, restrict_col, tokens = list(element.clauses)
+    return "(array_cat(%s, %s) @> %s)" % (
+        compiler.process(col, **kw),
+        compiler.process(restrict_col, **kw),
+        compiler.process(tokens, **kw))
 
 
 @compiles(Restrict, 'sqlite')
 def _sqlite_restrict(element: Restrict,
                      compiler: 'sa.Compiled', **kw: Any) -> str:
-    return "array_contains(%s)" % compiler.process(element.clauses, **kw)
+    col, _, tokens = list(element.clauses)
+    return "array_contains(%s, %s)" % (
+        compiler.process(col, **kw),
+        compiler.process(tokens, **kw))
