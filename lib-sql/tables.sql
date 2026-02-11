408fb24eab2183682aed81a1215f8953c1e0785d
Sarah Hoffmann <lonvia@denofr.de>
2026-02-11 10:40:11 +0100

    move osmline table definition into its own file

--- lib-sql/tables.sql
+++ lib-sql/tables.sql
@@ -43,28 +43,7 @@ CREATE TABLE location_property_tiger (
   linegeo GEOMETRY NOT NULL,
   postcode TEXT);
 
-drop table if exists location_property_osmline;
-CREATE TABLE location_property_osmline (
-    place_id BIGINT NOT NULL,
-    osm_id BIGINT NOT NULL,
-    parent_place_id BIGINT,
-    geometry_sector INTEGER NOT NULL,
-    indexed_date TIMESTAMP,
-    startnumber INTEGER,
-    endnumber INTEGER,
-    step SMALLINT,
-    partition SMALLINT NOT NULL,
-    indexed_status SMALLINT NOT NULL,
-    linegeo GEOMETRY NOT NULL,
-    address HSTORE,
-    token_info JSONB, -- custom column for tokenizer use only
-    postcode TEXT,
-    country_code VARCHAR(2)
-  ){{db.tablespace.search_data}};
-CREATE UNIQUE INDEX idx_osmline_place_id ON location_property_osmline USING BTREE (place_id) {{db.tablespace.search_index}};
-CREATE INDEX idx_osmline_geometry_sector ON location_property_osmline USING BTREE (geometry_sector) {{db.tablespace.address_index}};
-CREATE INDEX idx_osmline_linegeo ON location_property_osmline USING GIST (linegeo) {{db.tablespace.search_index}}
-  WHERE startnumber is not null;
+{% include('tables/interpolation.sql') %}
 
 drop table IF EXISTS search_name;
 {% if not db.reverse_only %}
