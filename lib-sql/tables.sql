0331b6adfd8066d6c8755d6633d557182896cfff
Sarah Hoffmann <lonvia@denofr.de>
2026-02-11 11:24:31 +0100

    move postcode table definition to separate file

--- lib-sql/tables.sql
+++ lib-sql/tables.sql
@@ -78,29 +78,8 @@ CREATE INDEX idx_place_addressline_place_id on place_addressline USING BTREE (pl
 DROP SEQUENCE IF EXISTS seq_place;
 CREATE SEQUENCE seq_place start 1;
 
--- Table for synthetic postcodes.
-DROP TABLE IF EXISTS location_postcodes;
-CREATE TABLE location_postcodes (
-  place_id BIGINT NOT NULL,
-  parent_place_id BIGINT,
-  osm_id BIGINT,
-  rank_search SMALLINT NOT NULL,
-  indexed_status SMALLINT NOT NULL,
-  indexed_date TIMESTAMP,
-  country_code varchar(2) NOT NULL,
-  postcode TEXT NOT NULL,
-  centroid GEOMETRY(Geometry, 4326) NOT NULL,
-  geometry GEOMETRY(Geometry, 4326) NOT NULL
-  );
-CREATE UNIQUE INDEX idx_location_postcodes_id ON location_postcodes
-  USING BTREE (place_id) {{db.tablespace.search_index}};
-CREATE INDEX idx_location_postcodes_geometry ON location_postcodes
-  USING GIST (geometry) {{db.tablespace.search_index}};
-CREATE INDEX IF NOT EXISTS idx_location_postcodes_postcode
-  ON location_postcodes USING BTREE (postcode, country_code)
-  {{db.tablespace.search_index}};
-CREATE INDEX IF NOT EXISTS idx_location_postcodes_osmid
-  ON location_postcodes USING BTREE (osm_id) {{db.tablespace.search_index}};
+
+{% include('tables/postcodes.sql') %}
 
 -- Table to store location of entrance nodes
 DROP TABLE IF EXISTS placex_entrance;
