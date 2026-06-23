7da48ea83fd18d8a16286c804b357ccec4419453
Sarah Hoffmann <lonvia@denofr.de>
2026-06-23 13:49:34 +0200

    implement mutation variants

--- src/nominatim_db/tokenizer/fuzzy_tokenizer/variants_mutate.py
+++ src/nominatim_db/tokenizer/fuzzy_tokenizer/variants_mutate.py
@@ -8,20 +8,55 @@
 Variant processor for regex mutations.
 """
 from typing import Optional, Any
+import re
 
 from icu import Transliterator
 
+from ...errors import UsageError
 from . import types as ttyp
 
 class MutationProcessor(ttyp.VariantProcessor):
 
     def __init__(self, config: dict[str, Any], rules: dict[str, Any],
                  normalizer: Transliterator) -> None:
-        pass
+        self.mode = config.get('mode', 'append')  # one of: append, replace
+        scope = config.get('scope', 'global')  # one of: global, variants
+
+        if scope == 'global':
+            self._internal_process_name = self._process_name_global
+        else:
+            raise UsageError(f"Unknown scope '{scope}' for mutation.")
+
+        if 'mutations' not in rules:
+            raise UsageError("Variant processor requires 'mutations' section.")
+        if not isinstance(rules['mutations'], list):
+            raise UsageError("'mutations' section in variant processor needs to be list.")
+        filters: list[tuple[str, str]] = []
+        for k, v in rules.items():
+            if k != 'mutations':
+                if not isinstance(v, (str, int)):
+                    raise UsageError('Variant attributes must be strings')
+                filters.append((k, str(v)))
+
+        self.filters = tuple(sorted(filters))
+
+        self.regexes = [(re.compile(r['pattern']), str(r['replacement']))
+                        for r in rules['mutations']
+                        if 'pattern' in r and 'replacement' in r]
 
     def get_filter_attributes(self) -> tuple[tuple[str, str], ...]:
-        return tuple()
+        return self.filters
 
     def process_name(self, name: str) -> Optional[list[str]]:
-        return None
+        return self._internal_process_name(name)
+
+    def _process_name_global(self, name: str) -> Optional[list[str]]:
+        mutated = name
+        for pattern, repl in self.regexes:
+            mutated = pattern.sub(repl, mutated)
+
+        if mutated == name:
+            return None
+
+        return [name, mutated] if self.mode == 'append' else [mutated]
 
